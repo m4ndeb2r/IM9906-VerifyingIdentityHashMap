e974a7eaa22f42d820825c018913068207320eda
Mattias Ulbrich <ulbrich@kit.edu>
2021-10-14 10:35:41 +0200

    proof for countEven

--- src/java/util/Specs.java
+++ src/java/util/Specs.java
@@ -2,6 +2,7 @@ class Specs {
 
     /*+KEY@ public normal_behaviour
       @  requires a != null;
+      @  requires a.length % 2 == 0;
       @  ensures \result == (\num_of \bigint i;
       @       0 <= i < a.length / (\bigint)2;
       @       a[2 * i] != null);
@@ -9,16 +10,28 @@ class Specs {
       @*/
     public static int countEven(/*@nullable*/Object[] a) {
         int result = 0;
-        for(int i = 0; 2*i < a.length; i += 2) {
+        
+        /*@ loop_invariant
+          @  result == (\num_of \bigint j;
+          @       0 <= j < i/2;
+          @       a[2 * j] != null);
+          @ loop_invariant 0 <= i <= a.length;
+          @ loop_invariant i%2 == 0;
+          @ decreases a.length + 1 - i;
+          @ assignable \strictly_nothing;
+          @*/
+        for(int i = 0; i < a.length; i += 2) {
             if(a[i] != null) {
                 result ++;
             }
         }
         return result;
+        
     }
     
     /*@ public normal_behaviour
       @  requires a != null;
+      @  requires a.length % 2 == 0;
       @  requires countEven(a) < a.length / 3;
       @  requires a.length >= 8;
       @  ensures (\exists int x, y; 0 <= x < y < a.length;
